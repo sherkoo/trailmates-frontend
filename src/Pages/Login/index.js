1b12fbc1adea056414af1e8a4d8bea0765288f75
s <stephenherko@gmail.com>
2023-05-09 15:00:18 -0500

    login form style updates

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Header from "../../Components/Header";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import Box from "@mui/material/Box";
 import Alert from "@mui/material/Alert";
 import { Link } from "react-router-dom";
 import Helpers from "../../Components/Helpers";
@@ -10,6 +9,8 @@ import vars from "../../Components/vars";
 import { useSelector } from "react-redux";
 import { Provider } from "react-redux";
 import store from "../../Redux/store";
+import { Grid, Box } from "@mui/material";
+import Card from "@mui/material/Card";
 
 const Login = () => {
   const userRedux = useSelector((state) => state.user);
@@ -40,7 +41,7 @@ const Login = () => {
   const handleAlert = () => {
     if (onAlert) {
       return (
-        <Alert severity="error">
+        <Alert severity="error" sx={{ marginBottom: "1em" }}>
           {vars.messages.login}
           {emailError ? <li>Email</li> : null}
           {passwordError ? <li>Password</li> : null}
@@ -49,48 +50,65 @@ const Login = () => {
     }
   };
 
-  if (!loggedIn) {
-    return (
-      <Provider store={store}>
-        <div data-testid="login-page">
-          <Header />
-          <h2>Login</h2>
-          <Box component="form" noValidate autoComplete="off">
-            <div>{handleAlert()}</div>
-            <div>
-              <TextField
-                error={emailError}
-                onChange={(event) => setEmailValue(event.target.value)}
-                id="outlined-basic"
-                label="Email"
-                variant="outlined"
-              />
+  return (
+    <Grid>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "cetner",
+          justifyContent: "center",
+        }}
+      >
+        <Card sx={{ padding: "1em", minWidth: "400px" }}>
+          <Provider store={store}>
+            <div data-testid="login-page">
+              <Header />
+              <h2>Login</h2>
+
+              <Box component="form" noValidate autoComplete="off">
+                <div>{handleAlert()}</div>
+                <div>
+                  <TextField
+                    error={emailError}
+                    sx={{ marginBottom: "1em" }}
+                    fullWidth
+                    onChange={(event) => setEmailValue(event.target.value)}
+                    id="outlined-basic"
+                    label="Email"
+                    variant="outlined"
+                  />
+                </div>
+                <div>
+                  <TextField
+                    sx={{ marginBottom: "1em" }}
+                    error={passwordError}
+                    fullWidth
+                    onChange={(event) => setPasswordValue(event.target.value)}
+                    id="outlined-basic"
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                  />
+                </div>
+                <div>
+                  <Button
+                    sx={{ textTransform: "none" }}
+                    onClick={handleSubmit}
+                    variant="contained"
+                  >
+                    Login
+                  </Button>
+                </div>
+                <div>
+                  Don't have an account? <Link to="/register">Register</Link>
+                </div>
+              </Box>
             </div>
-            <div>
-              <TextField
-                error={passwordError}
-                onChange={(event) => setPasswordValue(event.target.value)}
-                id="outlined-basic"
-                label="Password"
-                type="password"
-                variant="outlined"
-              />
-            </div>
-            <div>
-              <Button onClick={handleSubmit} variant="contained">
-                Login
-              </Button>
-            </div>
-            <div>
-              Don't have an account? <Link to="/register">Register</Link>
-            </div>
-          </Box>
-        </div>
-      </Provider>
-    );
-  } else {
-    return <div>Dashboard</div>;
-  }
+          </Provider>
+        </Card>
+      </Box>
+    </Grid>
+  );
 };
 
 export default Login;
